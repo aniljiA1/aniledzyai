be97df8e74e2981e5c929dbb98abfe55400e6202
Anil Kumar <126413189+aniljiA1@users.noreply.github.com>
2026-03-24 15:15:08 +0530

    Update api.ts

--- school-canteen/src/api/api.ts
+++ school-canteen/src/api/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://aniledzyai.onrender.com";
+
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: BASE_URL,
 });
